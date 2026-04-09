dbc386c3873589199e043a6cfba8e4e713cf9786
Piyush Pathare <patharepiyush111@gmail.com>
2026-04-09 22:26:16 +0530

    ConvesClient

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -3,7 +3,13 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+let convex: ConvexReactClient | null = null;
+
+if (typeof window !== "undefined") {
+  convex = new ConvexReactClient(
+    process.env.NEXT_PUBLIC_CONVEX_URL!
+  );
+}
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
   return <ConvexProvider client={convex}>{children}</ConvexProvider>;
